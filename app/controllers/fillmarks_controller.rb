8bed90e66007787ca3c68bd91ef474e19a5d48de
che <che.blancoo58@gmail.com>
2021-05-16 00:08:53 +0100

    Add update marks

--- app/controllers/fillmarks_controller.rb
+++ app/controllers/fillmarks_controller.rb
@@ -2,9 +2,6 @@ class FillmarksController < ApplicationController
   @@student_marks = nil
   def new
   end
-  def display_results
-    @display_result = @@student_marks
-  end
   def create
     exam_id  = Exam.where(["Subject_id = ? and name = ?", params[:subject_code], params[:sequence]]).first.id
     @student_marks =Mark.where(exam_id: exam_id)
@@ -14,4 +11,18 @@ class FillmarksController < ApplicationController
         redirect_to root_path
     end
   end
+  def results_with_no_marks
+    @test =[]
+    current_marks_to_fill = Mark.pluck(:id)
+      @tester =params.except(:controller,:action,:_method,:authenticity_token,:commit)
+      current_marks_to_fill.each  do |mark_id|
+        if @tester[:"#{mark_id}"]
+          @test << mark_id
+      end
+      @test.each do |data|
+        Mark.find(data.to_i).update_attribute(:grade,params[:"#{data}"])
+      end
+  end
+
 end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   resources :subjects
   resources :school_years
   resources :marks
+  get '/fillmarks', to: 'fillmarks#new'
+  get '/results', to: 'fillmarks#display_results', as: 'results'
+  put '/results_with_no_marks/',to: 'fillmarks#results_with_no_marks'
+  get '/results_with_no_marks/',to: 'fillmarks#results_with_no_marks'
+  post '/fillmarks', to: 'fillmarks#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
